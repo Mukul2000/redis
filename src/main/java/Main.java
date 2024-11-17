4168a938d5a5dc11fecdbcb04825280129a7f055
Mukul <mukulkumar627@gmail.com>
2024-11-17 06:24:32 +0000

    fix pings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,12 +21,14 @@ public class Main {
           // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
-
-          while (!serverSocket.isClosed()) {
-            clientSocket = serverSocket.accept();
-            InputStream in = clientSocket.getInputStream();
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-            clientSocket.close();
+          clientSocket = serverSocket.accept();
+          BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          while(true){
+            String line = in.readLine();
+            System.out.println(line);
+            if(line.equalsIgnoreCase("ping")){
+                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            }
           }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
