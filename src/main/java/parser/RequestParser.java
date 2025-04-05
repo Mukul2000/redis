33154cb77ec8f0e04e982ebfedb41b09aa8432df
mukul <mukulkumar627@gmail.com>
2025-04-05 13:51:09 +0000

    add RESP parser

---
+++ src/main/java/parser/RequestParser.java
@@ -0,0 +1,122 @@
+package parser;
+/*
+ * This class will receive input in RESP protocol possibly as a stream: *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n.
+ * It needs to return the command in array form: That's ["ECHO", "hey"]
+ * The first element is always the command. Arguments follow up after it.
+ * This class should only return the array, don't check if the arguments are enough. That's the executor's job.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import types.RedisArray;
+import types.RedisBoolean;
+import types.RedisBulkString;
+import types.RedisData;
+import types.RedisInteger;
+import types.RedisSimpleString;
+
+public class RequestParser {
+   private final InputStream inputStream; 
+
+   public RequestParser(InputStream inputStream) {
+      this.inputStream = inputStream;
+   }
+
+   public RedisData parse() throws IOException {
+      char c = readChar();
+
+      if (c == '*') {
+         // array
+         return parseArray();
+      }
+      else if (c == '+') {
+         return parseSimpleString();
+      }
+      else if (c == '$') {
+         // bulk string
+         return parseBulkString();
+      }
+      else if (c == '#') {
+         // boolean
+         return parseBoolean();
+      } 
+      else if (c == ':') {
+         return parseInteger();
+      }
+      return null;
+   }
+
+   private RedisData parseInteger() throws IOException {
+      return new RedisInteger(Integer.valueOf((int)readInteger()));
+   }
+
+   private RedisData parseArray() throws IOException {
+      int length = (int) readInteger();
+      RedisData[] values = new RedisData[length];
+      for (int i = 0; i < length; i++) {
+         RedisData element = parse();
+         values[i] = element;
+      }
+      return new RedisArray(values);
+   }
+
+   private RedisData parseBoolean() throws IOException {
+      return new RedisBoolean(readString().equals("t"));
+   }
+
+   private RedisData parseSimpleString() throws IOException {
+      return new RedisSimpleString(readString());
+   }
+
+   private RedisData parseBulkString() throws IOException {
+     long length = readInteger();
+
+     if (length < 0) {
+         skipNBytes(2);
+         return new RedisBulkString(null);
+     }
+     return new RedisBulkString(readString());
+   }
+
+   private long readInteger() throws IOException {
+        long value = 0;
+        boolean isNegative = false;
+        char c = readChar();
+        while (c != '\r') {
+            if (c == '-') isNegative = true;
+            else value = value * 10 + c - '0';
+            c = readChar();
+        }
+        skipNBytes(1);
+        if (isNegative) return -value;
+        else return value;
+    }
+
+   private char readChar() throws IOException {
+      int c = -1;
+      while (c == -1) c = inputStream.read(); // block until client sends data
+      return (char) c;
+   }
+   
+   private String readString() throws IOException {
+      StringBuilder sb = new StringBuilder();
+      while (true) {
+         char cur = readChar();
+         if (cur == '\r') {
+            break;
+         }
+
+         sb.append(cur);
+      }
+      skipNBytes(1); // skip the '\n' last character
+      return sb.toString();
+   }
+
+   private void skipNBytes(int n) throws IOException {
+      while (n > 0) {
+         readChar();
+         n--;
+      }
+   }
+}
---
+++ src/test/java/parser/RequestParserTest.java
@@ -0,0 +1,143 @@
+package parser;
+
+import org.junit.Test;
+
+import types.RedisArray;
+import types.RedisBoolean;
+import types.RedisBulkString;
+import types.RedisData;
+import types.RedisDataType;
+import types.RedisInteger;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import static org.mockito.Mockito.*;
+
+public class RequestParserTest {
+    @Test
+    public void parse_simple_string_success() throws IOException {
+        String command = "+OKLOL\r\n";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        RequestParser parser = new RequestParser(in);
+
+        RedisData parsed = parser.parse();
+
+        assertThat(parsed.getType()).isEqualTo(RedisDataType.SIMPLE_STRING);
+        assertThat(parsed.getFormattedValue()).isEqualTo(command);
+        assertThat(parsed.getRawValue()).isEqualTo("OKLOL");
+        assertThat(in.available()).isEqualTo(0);
+    }
+
+    @Test
+    public void parse_empty_simple_string_success() throws IOException {
+        String command = "+\r\n";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        RequestParser parser = new RequestParser(in);
+
+        RedisData parsed = parser.parse();
+
+        assertThat(parsed.getType()).isEqualTo(RedisDataType.SIMPLE_STRING);
+        assertThat(parsed.getFormattedValue()).isEqualTo(command);
+        assertThat(parsed.getRawValue()).isEqualTo("");
+        assertThat(in.available()).isEqualTo(0);
+    }
+
+    @Test
+    public void parse_data_type_stream_throws_exception_expectIOException() throws IOException {
+        InputStream mockInputStream = mock(InputStream.class);
+
+        when(mockInputStream.read()).thenThrow(new IOException("Simulated IO error"));
+        when(mockInputStream.read(any(byte[].class))).thenThrow(new IOException("Simulated IO error"));
+        when(mockInputStream.read(any(byte[].class), anyInt(), anyInt())).thenThrow(new IOException("Simulated IO error"));
+
+        RequestParser parser = new RequestParser(mockInputStream);
+
+        assertThrows(IOException.class, () -> parser.parse());
+    }
+
+    @Test
+    public void parse_bulk_string_success() throws IOException {
+        String command = "$4\r\ntest\r\n";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        RequestParser parser = new RequestParser(in);
+
+        RedisData parsed = parser.parse();
+
+        assertThat(parsed.getType()).isEqualTo(RedisDataType.BULK_STRING);
+        assertThat(parsed.getFormattedValue()).isEqualTo(command);
+        assertThat(parsed.getRawValue()).isEqualTo("test");
+        assertThat(in.available()).isEqualTo(0);
+    }
+
+    @Test
+    public void parse_empty_bulk_string_success() throws IOException {
+        String command = "$0\r\n\r\n";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        RequestParser parser = new RequestParser(in);
+
+        RedisData parsed = parser.parse();
+
+        assertThat(parsed.getType()).isEqualTo(RedisDataType.BULK_STRING);
+        assertThat(parsed.getFormattedValue()).isEqualTo(command);
+        assertThat(parsed.getRawValue()).isEqualTo("");
+        assertThat(in.available()).isEqualTo(0);
+    }
+
+    @Test
+    public void parse_integer_success() throws IOException {
+        String command = ":432\r\n";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        RequestParser parser = new RequestParser(in);
+
+        RedisData parsed = parser.parse();
+
+        assertThat(parsed.getType()).isEqualTo(RedisDataType.INTEGER);
+        assertThat(parsed.getFormattedValue()).isEqualTo(command);
+        assertThat(parsed.getRawValue()).isEqualTo(432);
+        assertThat(in.available()).isEqualTo(0);
+    }
+
+    @Test
+    public void parse_negative_integer_success() throws IOException {
+        String command = ":-432\r\n";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        RequestParser parser = new RequestParser(in);
+
+        RedisData parsed = parser.parse();
+
+        assertThat(parsed.getType()).isEqualTo(RedisDataType.INTEGER);
+        assertThat(parsed.getFormattedValue()).isEqualTo(command);
+        assertThat(parsed.getRawValue()).isEqualTo(-432);
+        assertThat(in.available()).isEqualTo(0);
+    }
+
+    @Test
+    public void parse_simple_array_success()  throws IOException {
+        String command = "*3\r\n#t\r\n$5\r\nhello\r\n:1234\r\n";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        RequestParser parser = new RequestParser(in);
+
+        RedisData parsed = parser.parse();
+
+        assertThat(parsed.getType()).isEqualTo(RedisDataType.ARRAY);
+        assertThat(parsed.getFormattedValue()).isEqualTo(command);
+        assertThat(in.available()).isEqualTo(0);       
+    }
+
+    @Test
+    public void parse_nested_array_success()  throws IOException {
+        String command = "*1\r\n*2\r\n:123\r\n$4\r\ntest\r\n";
+        InputStream in = new ByteArrayInputStream(command.getBytes());
+        RequestParser parser = new RequestParser(in);
+
+        RedisData parsed = parser.parse();
+
+        assertThat(parsed.getType()).isEqualTo(RedisDataType.ARRAY);
+        assertThat(parsed.getFormattedValue()).isEqualTo(command);
+        assertThat(in.available()).isEqualTo(0);       
+    }
+}
