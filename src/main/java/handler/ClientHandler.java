9ce0c1d421764d547dd9839332b31e2a5aadea6c
mukul <mukulkumar627@gmail.com>
2025-04-13 05:02:12 +0000

    clienthandler

--- src/main/java/handler/ClientHandler.java
+++ src/main/java/handler/ClientHandler.java
@@ -1,28 +1,50 @@
-package main.java.handler;
+package handler;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.Reader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.OutputStream;
 import java.net.Socket;
 
-public class ClientHandler {
-  public void handleClient(Socket clientSocket) {
+import parser.RequestParser;
+import types.RedisData;
+
+public class ClientHandler implements Runnable {
+  private Socket clientSocket;
+  private InputStream inputStream;
+  private OutputStream outputStream;
+
+  public ClientHandler(Socket clientSocket) {
+    this.clientSocket = clientSocket;
+  }
+
+  @Override
+  public void run() {
+    handleClient();
+  }
+
+  private void handleClient() {
     try {
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        while(true){
-        String line = in.readLine();
-        System.out.println(line);
-        if(line.equalsIgnoreCase("ping")){
-            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-        }
-        }
+      inputStream = clientSocket.getInputStream();
+      outputStream = clientSocket.getOutputStream();
+      RequestParser requestParser = new RequestParser(inputStream);
+      while (true) {
+        RedisData request = requestParser.parse();
+        System.out.println("Request: " + request);
+        if (request == null) continue;
+        RedisData response = RequestExecutor.execute(request);
+        System.out.println("Response: " + response);
+        outputStream.write(response.getFormattedValue().getBytes());
+        outputStream.flush();
+      }
     } catch (IOException e) {
-        System.out.println("lol");
+      System.out.println("lol");
+    } finally {
+      try {
+        if (clientSocket != null)
+          clientSocket.close();
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
     }
-
   }
 }
--- src/test/java/handler/ClientHandlerTest.java
+++ src/test/java/handler/ClientHandlerTest.java
@@ -1,19 +1,24 @@
 package handler;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.net.Socket;
 
 import org.junit.Test;
 
+import types.RedisBulkString;
+
 public class ClientHandlerTest {
     @Test
-    public void send_request_then_terminate_conn() throws IOException {
+    public void send_command_success() throws Exception {
         ByteArrayInputStream inputStream = new ByteArrayInputStream("*2\r\n$4\r\nECHO\r\n$5\r\nhello\r\n".getBytes());
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         Socket socket = mock(Socket.class);
@@ -21,11 +26,69 @@ public class ClientHandlerTest {
         when(socket.getOutputStream()).thenReturn(outputStream);
         ClientHandler clientHandler = new ClientHandler(socket);
 
-        clientHandler.run();
+        Thread clientThread = new Thread(clientHandler);
+        clientThread.start();
+
+        Thread.sleep(1000);
+        String response = outputStream.toString();
+        assertThat(response).isEqualTo("$5\r\nhello\r\n");
 
         inputStream.close();
+        outputStream.close();
+    }
+
+    @Test
+    public void send_successive_commands_success() throws Exception {
+        PipedInputStream inputStream = new PipedInputStream();
+        PipedOutputStream outputStreamToInput = new PipedOutputStream(inputStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStreamToInput.write("*2\r\n$4\r\nECHO\r\n$5\r\nhello\r\n".getBytes());
+
+        Socket socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(inputStream);
+        when(socket.getOutputStream()).thenReturn(outputStream);
+        ClientHandler clientHandler = new ClientHandler(socket);
+
+        Thread clientThread = new Thread(clientHandler);
+        clientThread.start();
+
+        Thread.sleep(1000);
+        String response = outputStream.toString();
+        assertThat(response).isEqualTo("$5\r\nhello\r\n");
+        outputStream.reset();
+
+        outputStreamToInput.write("*1\r\n$4\r\nPING\r\n".getBytes());
+        Thread.sleep(1000);
+        response = outputStream.toString();
+        assertThat(response).isEqualTo((new RedisBulkString("PONG")).getFormattedValue());
+
+        inputStream.close();
+        outputStream.close();
+        outputStreamToInput.close();
+    }
+
+    @Test
+    public void input_stream_throws_IOException_socket_closed() throws Exception {
+        final boolean[] isSocketClosed = {false};
+        ByteArrayInputStream inputStream = mock(ByteArrayInputStream.class);
+        Socket socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(inputStream);
+        when(socket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+        when(socket.isClosed()).thenReturn(false);
+        // doAnswer is for void methods
+        doAnswer(invocation -> {
+            isSocketClosed[0] = true;
+            return null;
+        }).when(socket).close();
+        when(socket.isClosed()).thenAnswer(invocation -> isSocketClosed[0]);
+        when(inputStream.read()).thenThrow(new IOException("something wrong bro"));
+
+        ClientHandler clientHandler = new ClientHandler(socket);
+        Thread clientThread = new Thread(clientHandler);
+        clientThread.start();
+
+        Thread.sleep(1000);
+        System.out.println(socket.isClosed());
         assertThat(socket.isClosed()).isTrue();
-        String output = outputStream.toString();
-        System.out.println("output: " + output);
     }
 }
