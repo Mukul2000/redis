87d3da2be27a299f1eaf267072bfc487194d6cf4
mukul <mukulkumar627@gmail.com>
2025-04-13 11:29:45 +0000

    add thread based implementation for posterity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import handler.ClientHandler;
 
 public class Main {
   public static void main(String[] args){
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
     int port = 6379;
     try {
       serverSocket = new ServerSocket(port);
@@ -16,22 +14,17 @@ public class Main {
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
       // Wait for connection from client.
-      while (true) {
-        // loop forever and listen for clients
-        clientSocket = serverSocket.accept();
-        final Socket socket = clientSocket;
-        System.out.println("Successful connection");
-        Thread clientThread = new Thread(() -> handleClient(socket));
-        // Start the thread. The main loop continues immediately.
-        clientThread.start();
-        System.out.println("Client started");
+      while (!serverSocket.isClosed() && !Thread.currentThread().isInterrupted()) {
+        Socket clientSocket = serverSocket.accept();
+        // Create a new thread for the client.
+        new Thread(new ClientHandler(clientSocket)).start();
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
       try {
-        if (clientSocket != null) {
-          clientSocket.close();
+        if (serverSocket != null) {
+          serverSocket.close();
         }
       } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
@@ -39,23 +32,6 @@ public class Main {
     }
   }
 
-  private static void handleClient(Socket clientSocket) {
-    try {
-      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      OutputStream outputStream = clientSocket.getOutputStream();
-      while (true) {
-        String line = in.readLine();
-        System.out.println("Command: " + line);
-        outputStream.write("+PONG\r\n".getBytes());
-      }
-    } catch (IOException e) {
-        // Catch IO errors during communication or closing for this specific client
-        System.err.println("IOException" + ": " + e.getMessage());
-    } finally {
-        // Socket and streams are guaranteed closed by try-with-resources, even if exceptions occurred.
-        System.out.println("Finished handling client");
-    }
-  }
 }
 
 
